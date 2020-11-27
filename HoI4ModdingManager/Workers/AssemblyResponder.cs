80e7fc4033a29ac94aa3e1c8dcfbf3fec8963997
Zakki0925224 <miyazakiyuuta15@gmail.com>
2020-11-27 22:36:04 +0900

    [Changed]: Changed anything

---
+++ HoI4ModdingManager/Workers/AssemblyResponder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoI4ModdingManager.Workers
+{
+    /// <summary>
+    /// アプリケーションのアセンブリ情報を返す
+    /// </summary>
+    class AssemblyResponder
+    {
+        /// <summary>
+        /// AssemblyTitleを返す
+        /// </summary>
+        /// <returns></returns>
+        public string RespondAssemblyTitle()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            return assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        }
+
+        /// <summary>
+        /// AssemblyCopyrightを返す
+        /// </summary>
+        /// <returns></returns>
+        public string RespondAssemblyCopyright()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            return assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
+        }
+
+        /// <summary>
+        /// AssembryVersionを返す
+        /// </summary>
+        /// <returns></returns>
+        public string RespondAssembryVersion()
+        {
+            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
+            Version version = assemblyName.Version;
+            return version.ToString();
+        }
+    }
+}
