ade5ab718486437032662c410b80b1a92b9a3ba9
Zakki0925224 <miyazakiyuuta15@gmail.com>
2020-12-05 21:44:44 +0900

    [Dummy]: Dummy commit

--- HoI4ModdingManager/Program.cs
+++ HoI4ModdingManager/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using HoI4ModdingManager.Managers.ModdingProjectManager.Forms;
+using System;
 using System.Windows.Forms;
-using HoI4ModdingManager.Managers.ModdingProjectManager.Forms;
 
 namespace HoI4ModdingManager
 {
---
+++ HoI4ModdingManager/common/Forms/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace HoI4ModdingManager.Forms
+{
+    public partial class AppSettings : Form
+    {
+        public AppSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
