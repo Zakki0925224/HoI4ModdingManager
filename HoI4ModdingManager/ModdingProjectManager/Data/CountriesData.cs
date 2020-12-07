d1a8adad78229caef64330ba21ce2570b4ade593
Zakki0925224 <miyazakiyuuta15@gmail.com>
2020-12-08 00:28:46 +0900

    [Add]: Adding methods

---
+++ HoI4ModdingManager/ModdingProjectManager/Data/CountriesData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace HoI4ModdingManager.ModdingProjectManager.Data
+{
+    class CountriesData
+    {
+        public int? id;
+        public DateTime? created_at;
+        public DateTime? updated_at;
+        public string country_tag;
+        public string country_name;
+        public string country_name_neutrality;
+        public string country_name_neutrality_def;
+        public string country_name_neutrality_adj;
+        public string country_name_democratic;
+        public string country_name_democratic_def;
+        public string country_name_democratic_adj;
+        public string country_name_fascism;
+        public string country_name_fascism_def;
+        public string country_name_fascism_adj;
+        public string country_name_communism;
+        public string country_name_communism_def;
+        public string country_name_communism_adj;
+        public string party_name_neutrality;
+        public string party_name_neutrality_long;
+        public string party_name_democratic;
+        public string party_name_democratic_long;
+        public string party_name_fascism;
+        public string party_name_fascism_long;
+        public string party_name_communism;
+        public string party_name_communism_long;
+        public int? capital_state_id;
+        public int? initial_teach_slot;
+        public int? initial_stability;
+        public int? initial_war_coop;
+        public int? initial_political_power;
+        public int? initial_transport;
+        public string graphic_culture;
+        public string initial_ideology;
+        public DateTime? last_election_at;
+        public int? election_interval;
+        public bool? is_no_election;
+
+        /// <summary>
+        /// データをすべてnullに置き換え
+        /// </summary>
+        public void DataReset()
+        {
+            id = null;
+            created_at = null;
+            updated_at = null;
+            country_tag = null;
+            country_name = null;
+            country_name_neutrality = null;
+            country_name_neutrality_def = null;
+            country_name_neutrality_adj = null;
+            country_name_democratic = null;
+            country_name_democratic_def = null;
+            country_name_democratic_adj = null;
+            country_name_fascism = null;
+            country_name_fascism_def = null;
+            country_name_fascism_adj = null;
+            country_name_communism = null;
+            country_name_communism_def = null;
+            country_name_communism_adj = null;
+            party_name_neutrality = null;
+            party_name_neutrality_long = null;
+            party_name_democratic = null;
+            party_name_democratic_long = null;
+            party_name_fascism = null;
+            party_name_fascism_long = null;
+            party_name_communism = null;
+            party_name_communism_long = null;
+            capital_state_id = null;
+            initial_teach_slot = null;
+            initial_stability = null;
+            initial_war_coop = null;
+            initial_political_power = null;
+            initial_transport = null;
+            graphic_culture = null;
+            initial_ideology = null;
+            last_election_at = null;
+            election_interval = null;
+            is_no_election = null;
+        }
+    }
+}
--- HoI4ModdingManager/ModdingProjectManager/Workers/ProjectImporter.cs
+++ HoI4ModdingManager/ModdingProjectManager/Workers/ProjectImporter.cs
@@ -5,12 +5,73 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using HoI4ModdingManager.Common.Workers;
+using HoI4ModdingManager.ModdingProjectManager.Data;
 using System.Data.SQLite;
 
 namespace HoI4ModdingManager.ModdingProjectManager.Workers
 {
     class ProjectImporter
     {
+        SQLiteConnection sqlc = null;
         
+        /// <summary>
+        /// データベース接続
+        /// </summary>
+        private void ConnectionDataBase(string dbFile)
+        {
+            dbFile = @"D:\software\sqlite3\test.hmp";
+
+            using (sqlc = new SQLiteConnection("Data Source=" + dbFile))
+            {
+                try
+                {
+                    sqlc.Open();
+                    // debug log
+                    Console.WriteLine("[SQLite]: Connenction successfull.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("[SQLite]: " + e.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// データベース切断
+        /// </summary>
+        private void DisconnectionDataBase()
+        {
+            if (sqlc == null)
+                return;
+
+            sqlc.Close();
+        }
+
+        /// <summary>
+        /// データを取得（一列のみ）
+        /// </summary>
+        /// <param name="tableName">テーブル名</param>
+        /// <param name="colmn">取得したい列番号(1~)</param>
+        /// <param name="cd">クラスインスタンス</param>
+        private void GetData(string tableName,int colmn, CountriesData cd)
+        {
+            if (sqlc == null)
+                return;
+
+            using (sqlc)
+            {
+                SQLiteCommand cmd = sqlc.CreateCommand();
+                cmd.CommandText = "SELECT * FROM" + tableName + " LIMIT " + colmn + ", 1";
+
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    cd.DataReset();
+                    // 変数に値を保存
+                    cd.id = (int?)reader.GetValue(0);
+
+                }
+            }
+        }
     }
 }
