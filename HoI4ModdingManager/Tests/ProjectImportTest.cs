0db41cfae98e3e00a919dcd75a650c69817477d4
Zakki0925224 <miyazakiyuuta15@gmail.com>
2020-12-25 17:44:15 +0900

    [Add]: Added log exporter

--- HoI4ModdingManager/Tests/ProjectImportTest.cs
+++ HoI4ModdingManager/Tests/ProjectImportTest.cs
@@ -18,11 +18,31 @@ namespace HoI4ModdingManager.Tests
             var countryData = data.CountryDataList;
             var ideologyData = data.IdeologyDataList;
 
+            Console.WriteLine("----Project Data----");
             Console.WriteLine(projectData[0].project_name);
             Console.WriteLine(projectData[0].created_at);
             Console.WriteLine(projectData[0].updated_at);
             Console.WriteLine(projectData[0].number_of_countries);
             Console.WriteLine(projectData[0].number_of_ideologies);
+
+            Console.WriteLine("----Country Data----");
+            
+            for (int i = 0; i < countryData.Count; i++)
+            {
+                Console.WriteLine("★" + countryData[i].country_name);
+                Console.WriteLine(countryData[i].id);
+                Console.WriteLine(countryData[i].country_tag);
+            }
+
+            Console.WriteLine("----Ideology Data----");
+
+            for (int i = 0; i < ideologyData.Count; i++)
+            {
+                Console.WriteLine("★" + ideologyData[i].ideology_name);
+                Console.WriteLine(ideologyData[i].color_r);
+                Console.WriteLine(ideologyData[i].color_g);
+                Console.WriteLine(ideologyData[i].color_b);
+            }
         }
     }
 }
