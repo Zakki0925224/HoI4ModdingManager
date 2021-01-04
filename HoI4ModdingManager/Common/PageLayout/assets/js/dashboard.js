5fff6b9684c2737e1cd05a28ba97e683c6d9c4d4
Zakki0925224 <zakki0925224@gmail.com>
2021-01-04 18:57:38 +0900

    [Add]: Added graph by chart.js

--- HoI4ModdingManager/Common/PageLayout/assets/js/dashboard.js
+++ HoI4ModdingManager/Common/PageLayout/assets/js/dashboard.js
@@ -12,4 +12,28 @@ function SetCountryData(country_name,
     document.getElementById("war_coop_count").textContent = initial_war_coop + "%";
     document.getElementById("transport_count").textContent = initial_transport;
     document.getElementById("ideology_text").textContent = initial_ideology;
+
+    // demo
+    SetPartySupportGraph();
+}
+
+function SetPartySupportGraph()
+{
+    var ctx = document.getElementById("party_support_graph");
+    var party_support_graph = new Chart
+    (
+        ctx,
+        {
+            type: 'doughnut',
+            data:
+            {
+                labels: ["Neutrality", "Democratic", "Fascism", "Communism"],
+                datasets:
+                [{
+                    backgroundColor: ["#7c7c7c", "#0000ff", "#964b00", "#ff0000"],
+                    data: [45, 32, 18, 5]
+                }]
+            }
+        }
+    );
 }
