fe01c56f0514f6e8ace0ad60ded1db85a482f9a0
Zakki0925224 <zakki0925224@gmail.com>
2021-01-25 22:31:56 +0900

    [Add]: Added new data container

---
+++ HoI4ModdingManager/ModdingProjectManager/NewDataContainer.cs
@@ -0,0 +1,24 @@
+﻿using HoI4ModdingManager.ModdingProjectManager.DataHangers.WillBeReplaced;
+using System.Collections.Generic;
+
+namespace HoI4ModdingManager.ModdingProjectManager
+{
+    class NewDataContainer
+    {
+        public NewDataContainer()
+        {
+            Initialize();
+        }
+
+        public ProjectDataHanger ProjectData { get; set; }
+        public List<CountryDataHanger> CountryData { get; set; }
+        public List<IdeologyDataHanger> IdeologyData { get; set; }
+
+        public void Initialize()
+        {
+            ProjectData = new ProjectDataHanger();
+            CountryData = new List<CountryDataHanger>();
+            IdeologyData = new List<IdeologyDataHanger>();
+        }
+    }
+}
