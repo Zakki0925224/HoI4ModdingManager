dbe25ae1fd64fde76941ebd042eafd15a4613a8a
Zakki0925224 <zakki0925224@gmail.com>
2021-02-07 22:49:45 +0900

    [Add]: Adding export process

---
+++ HoI4ModdingManager/ModdingProjectManager/ProjectExporter/WriteData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoI4ModdingManager.ModdingProjectManager.ProjectExporter
+{
+    class WriteData
+    {
+        private void WriteCountryData()
+        {
+
+        }
+    }
+}
