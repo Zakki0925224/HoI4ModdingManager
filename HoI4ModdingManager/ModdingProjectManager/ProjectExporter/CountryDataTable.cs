129ab683569cfb10356af7e41c64017fd755ba7f
Zakki0925224 <zakki0925224@gmail.com>
2021-02-08 22:31:28 +0900

    [Add]: Adding export process

--- HoI4ModdingManager/ModdingProjectManager/ProjectExporter/CountryDataTable.cs
+++ HoI4ModdingManager/ModdingProjectManager/ProjectExporter/CountryDataTable.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using HoI4ModdingManager.ModdingProjectManager.SQLite;
+using System;
 using System.Data.Linq.Mapping;
 using System.Linq;
-using HoI4ModdingManager.ModdingProjectManager.SQLite;
 
 namespace HoI4ModdingManager.ModdingProjectManager.ProjectExporter
 {
@@ -10,42 +10,61 @@ namespace HoI4ModdingManager.ModdingProjectManager.ProjectExporter
     {
         [Column(Name = "name", DbType = "TEXT")]
         public string Name { get; set; }
+
         [Column(Name = "country_tag", DbType = "TEXT")]
         public string CountryTag { get; set; }
+
         [Column(Name = "capital_state_id", DbType = "INTEGER")]
         public int CapitalStateID { get; set; }
+
         [Column(Name = "stability", DbType = "REAL")]
         public float Stability { get; set; }
+
         [Column(Name = "war_support", DbType = "REAL")]
         public float WarSupport { get; set; }
+
         [Column(Name = "political_power", DbType = "INTEGER")]
         public int PoliticalPower { get; set; }
+
         [Column(Name = "research_slots", DbType = "INTEGER")]
         public int ResearchSlots { get; set; }
+
         [Column(Name = "convoys", DbType = "INTEGER")]
         public int Convoys { get; set; }
+
         [Column(Name = "unit_file_id", DbType = "TEXT")]
         public string UnitFileID { get; set; }
+
         [Column(Name = "technologies", DbType = "TEXT")]
         public string Technologies { get; set; }
+
         [Column(Name = "ideas", DbType = "TEXT")]
         public string Ideas { get; set; }
+
         [Column(Name = "ruling_party_ideology", DbType = "TEXT")]
         public string RulingPartyIdeology { get; set; }
+
         [Column(Name = "last_election_at", DbType = "BLOB")]
         public DateTime LastElectionAt { get; set; }
+
         [Column(Name = "is_allow_election", DbType = "INTEGER")]
         public int IsAllowElection { get; set; }
+
         [Column(Name = "party_supports", DbType = "TEXT")]
         public string PartySupports { get; set; }
+
         [Column(Name = "rgb_country_color", DbType = "TEXT")]
         public string RGBCountryColor { get; set; }
+
         [Column(Name = "graphic_culture", DbType = "TEXT")]
         public string GraphicCulture { get; set; }
+
         [Column(Name = "country_names", DbType = "TEXT")]
         public string CountryNames { get; set; }
+
         [Column(Name = "party_names", DbType = "TEXT")]
         public string PartyNames { get; set; }
+
         [Column(Name = "country_flag_paths", DbType = "TEXT")]
         public string CountryFlagPaths { get; set; }
 
--- HoI4ModdingManager/ModdingProjectManager/ProjectExporter/ProjectDataTable.cs
+++ HoI4ModdingManager/ModdingProjectManager/ProjectExporter/ProjectDataTable.cs
@@ -7,24 +7,34 @@ namespace HoI4ModdingManager.ModdingProjectManager.ProjectExporter
     {
         [Column(Name = "project_name", DbType = "TEXT")]
         public string ProjectName { get; set; }
+
         [Column(Name = "mod_version", DbType = "TEXT")]
         public string ModVersion { get; set; }
+
         [Column(Name = "supported_game_version", DbType = "TEXT")]
         public string SupportedGameVersion { get; set; }
+
         [Column(Name = "tags", DbType = "TEXT")]
         public string Tags { get; set; }
+
         [Column(Name = "thumbnail_picture_path", DbType = "TEXT")]
         public string ThumbnailPicturePath { get; set; }
+
         [Column(Name = "replace_paths", DbType = "TEXT")]
         public string ReplacePaths { get; set; }
+
         [Column(Name = "mod_path", DbType = "TEXT")]
         public string ModPath { get; set; }
+
         [Column(Name = "user_dir", DbType = "TEXT")]
         public string UserDir { get; set; }
+
         [Column(Name = "remote_file_id", DbType = "TEXT")]
         public string RemoteFileID { get; set; }
+
         [Column(Name = "depondency_mods", DbType = "TEXT")]
         public string DepondencyMods { get; set; }
+
         [Column(Name = "depondency_dlcs", DbType = "TEXT")]
         public string DepondencyDLCs { get; set; }
 
